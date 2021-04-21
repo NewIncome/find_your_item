9b5862700dbc19ebfb10c5afffff2f3a28f31178
NewIncome <alfredo_cardenas@live.com>
2021-04-21 17:23:48 -0500

    Fix bug in toDateTime helper func

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,8 +5,12 @@ const getItemID = itemDiv => Number(itemDiv.getAttribute('data-index')) + 1;
 const curItemName = (items, id) => items.find(itm => itm.id === id).name;
 
 const toDateTime = dt => {
-  if (typeof dt === 'string' && dt.slice(0, 10).match(/-/g).length === 2) {
-    return `${dt.slice(0, 10)} ${dt.slice(11, 16)}`;
+  console.log('in Utils');
+  console.log(dt);
+  if (typeof dt === 'string' && dt.slice(0, 10).match(/-/g)) {
+    if (dt.slice(0, 10).match(/-/g).length === 2) {
+      return `${dt.slice(0, 10)} ${dt.slice(11, 16)}`;
+    }
   }
   return dt;
 };
